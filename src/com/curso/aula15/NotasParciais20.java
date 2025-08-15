1c8b2bd6517c1838e1930817bf6cbb4113ff4aee
Lucas Oliveira <lucasdthe@gmail.com>
2025-08-15 20:51:38 -0300

    Programa que verifica o sistema de numeracao decimal

---
+++ src/com/curso/aula15/NotasParciais20.java
@@ -0,0 +1,19 @@
+package com.curso.aula15;
+import java.util.Scanner;
+
+public class NotasParciais20 {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Primeira nota: ");
+        double primeiraNota = scan.nextDouble();
+
+        System.out.print("Segunda nota: ");
+        double segundaNota = scan.nextDouble();
+
+        System.out.print("Terceira nota: ");
+        double terceiraNota = scan.nextDouble();
+    }
+}
